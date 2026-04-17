18809242021f5efe77cccb851b2f25b4c2895cc7
Rodolfo soto <rod.sotod@duocuc.cl>
2026-04-16 21:39:33 -0400

    comienzo de microservicio

---
+++ src/main/java/com/example/LaFachada/LaFachadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LaFachada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaFachadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaFachadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LaFachada/LaFachadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LaFachada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaFachadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
