369278120c05a951930f323725be2019c7dcfb47
Orlando <169166413+OrlandoDiaz2003@users.noreply.github.com>
2026-07-01 18:40:17 -0400

    UPDATE: Configuracion con S3 real AWS

--- src/main/java/com/example/LaFachada/Config/S3Config.java
+++ src/main/java/com/example/LaFachada/Config/S3Config.java
@@ -1,31 +1,31 @@
 package com.example.LaFachada.Config;
 
-import java.net.URI;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
+    @Value("${aws.access.key.id}")
+    private String accessKey;
 
-    @Value("${AWS_S3_ENDPOINT:http://localhost:4566}")
-    private String s3Endpoint;
+    @Value("${aws.secret.access.key}")
+    private String secretKey;
+
+    @Value("${aws.session.token}")
+    private String sessionToken;
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .endpointOverride(URI.create(s3Endpoint))
                 .region(Region.US_EAST_1)
                 .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("test", "test")
-                ))
-                .forcePathStyle(true)
+                        AwsSessionCredentials.create(accessKey, secretKey, sessionToken)))
                 .build();
     }
 }
