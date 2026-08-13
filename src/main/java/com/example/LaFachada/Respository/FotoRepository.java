5f58059eacf0aa16c1110f4a66d6d63dc22897ef
sotom28 <rod.sotod@duocuc.cl>
2026-08-12 21:44:15 -0400

    update: prpiedad lista   actualizar ecr 3

--- src/main/java/com/example/LaFachada/Respository/FotoRepository.java
+++ src/main/java/com/example/LaFachada/Respository/FotoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.example.LaFachada.Model.Foto;
 
+
 @Repository
 public interface FotoRepository extends JpaRepository<Foto, Integer>{
 
